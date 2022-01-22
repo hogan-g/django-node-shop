7f88f0cdbad02971631eb763be98884bf1859204
Gareth Hogan <gareth.hogan22@mail.dcu.ie>
2022-01-22 11:26:46 +0000

    Made basic data models for shop, started basic html base and hello file

--- backend/garyshop/admin.py
+++ backend/garyshop/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(API_user)
+admin.site.register(Product)
+admin.site.register(Basket)
+admin.site.register(BasketItem)
+admin.site.register(Order)
--- backend/garyshop/views.py
+++ backend/garyshop/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    # get the request in
+    # do some logic with models in database
+    # return a webpage to the user
+    dic = {'name':'Gareth', 'Title':'Mstr.'}
+    names = ['Tinky Winky', 'Dipsy', 'Laa-Laa', 'Po']
+    return render(request, 'index.html', {'names':names })
