cf01a5360d4aff3ea69284c29c42668063e878b4
Gareth Hogan <gareth.hogan22@mail.dcu.ie>
2022-01-24 11:28:09 +0000

    moved hello page to act as home page, added button to access admin page

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', views.index),
+    path('', views.index),
 ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
